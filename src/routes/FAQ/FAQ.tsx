b74cb1382edc78e3670e84f86d4605622ea6ff97
manuellarinaldi <manuellarinaldi@gmail.com>
2026-04-10 10:01:16 -0300

    feat: Criando a estrutura .tsx da página faq.

--- src/routes/FAQ/FAQ.tsx
+++ src/routes/FAQ/FAQ.tsx
@@ -1,5 +1,90 @@
 export default function FAQ() {
   return (
-    <div>FAQ</div>
-  )
-}
+        <main>
+      {/* Sobre a ONG */}
+      <section>
+        <h2>Sobre a ONG</h2>
+        <p>
+          <strong>O que é a ONG Turma do Bem?</strong> A ONG Turma do Bem é uma
+          organização sem fins lucrativos que oferece atendimento odontológico
+          gratuito a crianças e adolescentes em situação de vulnerabilidade
+          social, especialmente aqueles com graves problemas dentários.
+        </p>
+        <p>
+          <strong>Onde a ONG atua?</strong> A ONG Turma do Bem atua em todos os
+          estados brasileiros e mais 12 países da América Latina e Portugal.
+        </p>
+      </section>
+
+      {/* Voluntariado */}
+      <section>
+        <h2>Voluntariado</h2>
+        <p>
+          <strong>Como posso me tornar voluntariado?</strong> Se inscrevendo,
+          responda nosso formulário e se torne um voluntariado!
+        </p>
+        <p>
+          <strong>Preciso ter consultório próprio para participar?</strong> Sim,
+          é preciso ter consultório próprio para participar do voluntariado na
+          Turma do Bem.
+        </p>
+      </section>
+
+      {/* Doações */}
+      <section>
+        <h2>Doações</h2>
+        <p>
+          <strong>Posso doar materiais odontológicos?</strong> Sim! você pode
+          doar materiais odontológicos.
+        </p>
+        <p>
+          <strong>Posso fazer doações mensais/recorrentes?</strong> Sim! é
+          possível fazer doações mensais ou recorrentes para a Turma do Bem.
+        </p>
+      </section>
+
+      {/* Beneficiários */}
+      <section>
+        <h2>Beneficiários</h2>
+        <p>
+          <strong>O atendimento é totalmente gratuito?</strong> Sim! você não
+          terá nenhum tipo de gasto.
+        </p>
+        <p>
+          <strong>Como posso inscrever alguém para receber atendimento?</strong>{" "}
+          Acesse o site da ONG e realize o pré-cadastro.
+        </p>
+      </section>
+
+      {/* Dúvidas e Parcerias */}
+      <section>
+        <h2>Dúvidas e Parcerias</h2>
+        <p>
+          <strong>Empresas podem fazer parceria com a Turma do Bem?</strong>{" "}
+          Sim, empresas podem fazer parcerias com a Turma do Bem.
+        </p>
+        <p>
+          <strong>Instituições de ensino podem colaborar com a ONG?</strong>{" "}
+          Sim, instituições de ensino podem colaborar com a ONG Turma do Bem.
+        </p>
+      </section>
+
+      {/* Outras Dúvidas */}
+      <section>
+        <h2>Outras Dúvidas</h2>
+        <p>
+          <strong>
+            A ONG Turma do Bem atende apenas adolescentes ou também adultos e
+            crianças?
+          </strong>{" "}
+          A ONG Turma do Bem atende exclusivamente adolescentes de 11 a 17
+          anos, além de mulheres vítimas de violência.
+        </p>
+        <p>
+          <strong>A ONG realiza eventos ou campanhas?</strong> Sim! a ONG
+          realiza eventos e campanhas em escolas.
+        </p>
+      </section>
+    </main>
+  );
+};
